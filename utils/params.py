9848856b8462dcab4ab467744fb0a9479cf3e28d
elnaz roshan <el.roshan@protonmail.com>
2025-07-30 16:31:34 +0200

    params added

--- utils/params.py
+++ utils/params.py
@@ -1,3 +1,19 @@
 import os
 
 api_key = os.getenv("API_KEY")
+
+values_keywords = {"quality": ["product quality", "speed", "accessibility", "reliability", "aesthetics", "helpful",
+                        "efficiency", "convenience", "flexibility", "stability", "functional", "affordable", "valuable"],
+           "innovation": ["digital", "innovation", "technology", "automation", "disruption",
+                          "modernity", "scalability", "data-driven", "cutting-edge", "ai", "inventive"],
+           "user_centricity": ["customer orientation", "customer service", "user experience", "satisfaction",
+                               "customization", "proactivity", "supportiveness", "personalization", "engagement", "support", "safety", "secure", "protection", "tailored"],
+           "empowerment": ["self-determination", "financial independence", "control", "education",
+                           "freedom", "choice", "autonomy", "empowerment", "international"
+                           "wealth", "independence", "planning", "smart", "bravery"],
+           "community": ["inclusion", "diversity", "global access", "local relevance",
+                         "community", "cooperation", "collaboration", "social"],
+           "ethos": ["fairness", "transparency", "trust", "sustainability", "responsibility", "impact",
+                     "integrity", "accountability", "honesty", "parity", "sincerity", "ethics", "credibility"],
+           "simplicity": ["simplicity", "simple", "ease", "clarity", "intuitive", "user-friendly", "clean", "smooth"],
+           "growth": ["ambition", "growth", "expansion", "goal", "vision", "future", "development"]}
