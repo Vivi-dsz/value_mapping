1eac3750be23455ed733e7ced9476ae65f483870
Ibrahim Eksi <95856789+ibraeksi@users.noreply.github.com>
2025-07-30 18:40:15 +0200

    added simple topic prediction model to app

--- backend/package/api_file.py
+++ backend/package/api_file.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
-import pickle
-
-from backend.package.dummy import calculate_function
+from backend.package.predict_topic import predict_topic
 
 # FastAPI instance
 app = FastAPI()
@@ -12,10 +10,23 @@ def root():
     return {'greeting':"hello"}
 
 # Prediction endpoint
-@app.get("/calculate")
-def predict(bankname):
+@app.get("/predict")
+def predict(user_review):
     # Use the function in our package to run the prediction
-    df = calculate_function(bankname)
+    prediction = predict_topic(user_review)
 
-    # Return figure
-    return {'pred': float(df)}
+    # Return prediction
+    return {"prediction": prediction}
+
+
+#### Following code is for using with the dummy.py function
+#### to show the API can read csv file in the data folder
+# from backend.package.dummy import calculate_function
+# # Calculate endpoint
+# @app.get("/calculate")
+# def calculate(bankname):
+#     # Use the function in our package to run the prediction
+#     df = calculate_function(bankname)
+
+#     # Return figure
+#     return {'pred': float(df)}
