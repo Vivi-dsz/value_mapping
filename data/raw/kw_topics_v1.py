5b297afb7401a15f3a2969385fb8b45b9cce6fea
Liana Gilmanova <liana.gilmanova@gmail.com>
2025-08-01 15:33:59 +0200

    updated

--- data/raw/kw_topics_v1.py
+++ data/raw/kw_topics_v1.py
@@ -1,15 +1,31 @@
-kw_dict = {"quality": ["product quality", "speed", "accessibility", "reliability", "aesthetics", "helpful",
-                        "efficiency", "convenience", "flexibility", "stability", "functional", "affordable", "valuable"],
-           "innovation": ["digital", "innovation", "technology", "automation", "disruption",
-                          "modernity", "scalability", "data-driven", "cutting-edge", "ai", "inventive"],
-           "user_centricity": ["customer orientation", "customer service", "user experience", "satisfaction",
-                               "customization", "proactivity", "supportiveness", "personalization", "engagement", "support", "safety", "secure", "protection", "tailored"],
-           "empowerment": ["self-determination", "financial independence", "control", "education",
-                           "freedom", "choice", "autonomy", "empowerment", "international"
-                           "wealth", "independence", "planning", "smart", "bravery"],
-           "community": ["inclusion", "diversity", "global access", "local relevance",
-                         "community", "cooperation", "collaboration", "social"],
-           "ethos": ["fairness", "transparency", "trust", "sustainability", "responsibility", "impact",
-                     "integrity", "accountability", "honesty", "parity", "sincerity", "ethics", "credibility"],
-           "simplicity": ["simplicity", "simple", "ease", "clarity", "intuitive", "user-friendly", "clean", "smooth"],
-           "growth": ["ambition", "growth", "expansion", "goal", "vision", "future", "development"]}
+kw_dict = {
+  "quality_usability": [
+    "quality", "reliability", "accessibility",  "easy",
+    "functionality", "intuitive", "usability", "seamless",
+    "affordable", "valuable", "flexibility", "smooth", "simple"
+  ],
+  "innovation_technology": [
+    "innovation", "digital", "technology", "automation", "ai", "data",
+    "realtime", "cloud", "learn", "smart"
+  ],
+  "trust_ethics": [
+    "trust", "transparency", "accountability", "responsibility",
+    "compliance", "security", "protection",  "secure",
+    "protected", "sustainable", "safety", "impact"
+  ],
+  "empowerment_control": [
+    "control", "freedom", "choice", "confidence", "insight",
+    "choose", "personal", "empowerment", "goal"
+  ],
+  "user_centricity_support": [
+    "support", "service", "experience", "care", "clarity", "enjoy",
+    "people", "tailored"
+  ],
+  "community_belonging": [
+    "community", "sharing", "global", "worldwide", "abroad", "social"
+  ],
+  "growth_ambition": [
+    "growth", "ambition", "development", "expansion", "vision",
+    "future", "progress", "opportunity", "earn", "free"
+  ]
+}
