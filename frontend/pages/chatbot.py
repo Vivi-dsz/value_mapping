f37335dc660e22a8f057451160b6e1d60fdd9c28
Ibrahim Eksi <95856789+ibraeksi@users.noreply.github.com>
2025-08-05 19:00:45 +0200

    updated frontend

--- frontend/pages/chatbot.py
+++ frontend/pages/chatbot.py
@@ -1,6 +1,21 @@
 import streamlit as st
-from modules.navbar import navbar
+import os, sys
+import pandas as pd
 
+rootpath = os.path.join(os.getcwd(), '..')
+sys.path.append(rootpath)
+review_merged_data = os.path.join(rootpath, 'data/preprocessed/', 'merged_review_topics_v01.csv')
+
+from frontend.modules.navbar import navbar
+from backend.models.agent import *
+from backend.preprocess.brand_count_function import *
+from backend.preprocess.review_count_function import *
+from data.raw.brands_about_us import brand_text
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("OpenAI_API_KEY")
 
 def chatbot():
     st.session_state.update(st.session_state)
@@ -13,11 +28,50 @@ def chatbot():
         initial_sidebar_state="expanded"
     )
 
-    st.write(
-        """This demo illustrates a combination of plotting and animation with
-    Streamlit. We're generating a bunch of random numbers in a loop for around
-    5 seconds. Enjoy!"""
-    )
+    review_merged_df = pd.read_csv(review_merged_data)
+
+    @st.cache_data
+    def load_keyword_dfs():
+        brand_kw_df = get_brand_counts(brand_text, keywords)
+        review_kw_df = get_review_counts(review_merged_df, keywords)
+        return brand_kw_df, review_kw_df
+
+    brand_kw_df, review_kw_df = load_keyword_dfs()
+
+    # Streamlit app setup
+    st.title("💬 Brand Strategy Assistant")
+    st.caption("🚀 A brand expert chatbot powered by OpenAI")
+    st.markdown("""I analyze brand perception, user sentiment, and market positioning
+                to support your strategy. Just name a brand or challenge —
+                I’ll start with a quick diagnosis and offer more detail if needed.
+                """)
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+        st.session_state["messages"] = [{"role": "assistant", "content": "Ready to shape your next branding move?"}]
+
+    if "last_brands" not in st.session_state:
+        st.session_state.last_brands = []
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+        # Call your custom agent
+        msg = handle_query(
+            question = prompt,
+            brand_kw_df=brand_kw_df,
+            review_kw_df=review_kw_df,
+            api_key=api_key,
+            chat_history=st.session_state.messages,
+            last_brands=st.session_state.last_brands
+            )
+
+        st.session_state.messages.append({"role": "assistant", "content": msg})
+        st.chat_message("assistant").write(msg)
+
 
 if __name__ == '__main__':
     chatbot()
--- frontend/pages/customeranalysis.py
+++ frontend/pages/customeranalysis.py
@@ -1,5 +1,21 @@
 import streamlit as st
-from modules.navbar import navbar
+import os, sys
+import pandas as pd
+
+rootpath = os.path.join(os.getcwd(), '..')
+sys.path.append(rootpath)
+user_reviews_data = os.path.join(rootpath, 'data/raw/', 'user_reviews_10k_v02.csv')
+user_reviews_processed_data = os.path.join(rootpath, 'data/preprocessed/', 'kw_counted_user_reviews_v01.csv')
+user_reviews_sentiment_data = os.path.join(rootpath, 'data/preprocessed/', 'final_reviews_with_topics_and_sentiment.csv')
+
+from frontend.modules.navbar import navbar
+from backend.visualization.get_kw_count_df import get_kw_count_df
+from backend.visualization.get_user_kw_count_df import get_user_kw_count_df
+from data.raw.kw_topics_v1 import kw_dict
+from data.raw.brands_about_us import brand_text
+from backend.visualization.kw_count_polar_plot import kw_count_polar_plot
+from backend.visualization.sentiment_heatmap import sentiment_heatmap
+from backend.visualization.monthly_sentiment_plot import monthly_sentiment_plot
 
 
 def customeranalysis():
@@ -13,11 +29,43 @@ def customeranalysis():
         initial_sidebar_state="expanded"
     )
 
-    st.write(
-        """This demo illustrates a combination of plotting and animation with
-    Streamlit. We're generating a bunch of random numbers in a loop for around
-    5 seconds. Enjoy!"""
-    )
+    st.header("How your customers are perceiving your brand")
+
+    left_top, gap_1, right_top = st.columns([5.5, 0.5, 4], vertical_alignment="top")
+
+    brand_name = st.session_state['chosen_brand']
+
+    with left_top:
+        if brand_name is not None:
+            df = get_kw_count_df(kw_dict, brand_text)
+            df['strategy'] = df['brand']
+
+            ### Use keyword matching (takes longer to execute on app)
+            # user_reviews_df = get_user_kw_count_df(pd.read_csv(user_reviews_data), kw_dict)
+            # user_reviews_df['strategy'] = 'Customer'
+
+            ### Use already keyword matched data directly
+            user_reviews_df = pd.read_csv(user_reviews_processed_data)
+            user_reviews_df['strategy'] = 'Customer'
+
+            ### Use preprocessed data with topics from model
+            # user_reviews = pd.read_csv(user_reviews_sentiment_data)
+            # user_reviews_df = user_reviews.groupby(['topic', 'app']).agg({'reviewId': 'count'}).reset_index().rename(columns={'topic': 'category', 'app': 'brand', 'reviewId': 'count'})
+            # user_reviews_df['strategy'] = 'Customer'
+
+            df = pd.concat([df, user_reviews_df]).reset_index(drop=True)
+
+            fig = kw_count_polar_plot(df, kw_dict, brand_text, [brand_name], customer=True)
+            st.plotly_chart(fig)
+    with right_top:
+        if brand_name is not None:
+            st.subheader("Perception Gap Analysis")
+
+    sentiment_data = pd.read_csv(user_reviews_sentiment_data)
+
+    if brand_name is not None:
+        st.plotly_chart(sentiment_heatmap(sentiment_data))
+        st.plotly_chart(monthly_sentiment_plot(sentiment_data, brand_name))
 
 if __name__ == '__main__':
     customeranalysis()
