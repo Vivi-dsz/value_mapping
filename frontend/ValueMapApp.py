11681958757820f5afb03932ed6706be909f72ae
Ibrahim Eksi <95856789+ibraeksi@users.noreply.github.com>
2025-08-07 09:18:18 +0200

    added url to final app

--- frontend/ValueMapApp.py
+++ frontend/ValueMapApp.py
@@ -8,37 +8,44 @@ from pages.aboutus import aboutus
 
 
 def main():
-    st.session_state.update(st.session_state)
+    with st.form("finalapp"):
+        st.session_state.update(st.session_state)
 
-    #--- Initialize session_state
-    if 'active_page' not in st.session_state:
-        st.session_state.active_page = 'Welcome'
-        st.session_state['chosen_brand'] = None
+        #--- Initialize session_state
+        if 'active_page' not in st.session_state:
+            st.session_state.active_page = 'Welcome'
+            st.session_state['chosen_brand'] = None
 
-    # Get current values of states
-    st.session_state.active_page = st.session_state.active_page
-    st.session_state['chosen_brand'] = st.session_state['chosen_brand']
+        # Get current values of states
+        st.session_state.active_page = st.session_state.active_page
+        st.session_state['chosen_brand'] = st.session_state['chosen_brand']
 
-    st.set_page_config(
-        page_title="Value Mapping",
-        page_icon=":moneybag:",
-        layout="wide",
-        initial_sidebar_state="expanded"
-    )
+        st.set_page_config(
+            page_title="Value Mapping",
+            page_icon=":moneybag:",
+            layout="wide",
+            initial_sidebar_state="expanded"
+        )
 
-    #--- Run the active page
-    if st.session_state.active_page == 'Welcome':
-        welcome()
-    elif st.session_state.active_page == 'Brand Analysis':
-        brandanalysis()
-    elif st.session_state.active_page == 'Competition Analysis':
-        companalysis()
-    elif st.session_state.active_page == 'Customer Analysis':
-        customeranalysis()
-    elif st.session_state.active_page == 'AI Chatbot':
-        chatbot()
-    elif st.session_state.active_page == 'About Us':
-        aboutus()
+        #--- Run the active page
+        if st.session_state.active_page == 'Welcome':
+            welcome()
+        elif st.session_state.active_page == 'Brand Analysis':
+            brandanalysis()
+        elif st.session_state.active_page == 'Competitor Analysis':
+            companalysis()
+        elif st.session_state.active_page == 'Customer Analysis':
+            customeranalysis()
+        elif st.session_state.active_page == 'AI Chatbot':
+            chatbot()
+        elif st.session_state.active_page == 'About Us':
+            aboutus()
 
 if __name__ == '__main__':
+
+    # Change this URL to the one of your API
+    API_URL = "https://finalapp-186737174934.europe-west10.run.app"
+
+    url = f"{API_URL}/finalapp"
+
     main()
