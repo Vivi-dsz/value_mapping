f1a45b5070a0d72ec90030a89e5ecfe4a8be8ba8
Vivien Daszkowski <viviendaszkowski@outlook.de>
2025-08-07 16:09:16 +0200

    updated frontend

--- frontend/pages/aboutus.py
+++ frontend/pages/aboutus.py
@@ -9,6 +9,8 @@ imagepath_ibraeksi = Path(__file__).parents[1] / '/data/images/ibraeksi.jpg'
 imagepath_gilmali = Path(__file__).parents[1] / '/data/images/gilmali.png'
 imagepath_elnroshan = Path(__file__).parents[1] / '/data/images/elnroshan.jpg'
 
+imagelist = [imagepath_vivi,imagepath_ibraeksi, imagepath_gilmali, imagepath_elnroshan]
+
 def aboutus():
     st.session_state.update(st.session_state)
     navbar()
@@ -28,7 +30,7 @@ def aboutus():
         {
             "title": "Chief Executive Officer (CEO)",
             "name": "Vivien Daszkowski",
-            "image": imagepath_vivi,
+            #"image": imagepath_vivi,
             "In-Project Role": "Founder of the core idea, led the presentation, and established the main workflows for the team.",
             "Real-World Role":
             """Vivien did her Master’s degree in Management and worked at the intersection of business and tech
@@ -39,7 +41,7 @@ def aboutus():
         {
             "title": "Chief Technology Officer (CTO)",
             "name": "Ibrahim Eksi",
-            "image": imagepath_ibraeksi,
+            #"image": imagepath_ibraeksi,
             "In-Project Role": "Owns the scraping pipeline setup, frontend, and supports the broader technical infrastructure.",
             "Real-World Role":
             """Ibrahim is a Mechanical Engineer with 6 years of experience in vehicle safety simulations followed by 2+ years experience as a data analyst.""",
@@ -49,7 +51,7 @@ def aboutus():
         {
             "title": "Chief Product Officer (CPO)",
             "name": "Liana Gilmanova",
-            "image": imagepath_gilmali,
+            #"image": imagepath_gilmali,
             "In-Project Role": "Focused on product development — created the system diagram and proposed the initial MVP structure.",
             "Real-World Role":
             """Liana is a Product Manager with 8 years experience in building and scaling Customer facing Products across 18 countries
@@ -60,7 +62,7 @@ def aboutus():
         {
             "title": "Chief Innovation Officer (CIO)",
             "name": "Elnaz Roshan, Ph.D.",
-            "image": imagepath_elnroshan,
+            #"image": imagepath_elnroshan,
             "In-Project Role": "Responsible for the AI model development and integration, ensuring metrics and functions are intelligent.",
             "Real-World Role":
             """Elnaz is a Climate Economics with 10+ years of experience in evaluating climate transition
@@ -84,7 +86,7 @@ def aboutus():
             with cols[idx]:
                 left, right = st.columns([2, 4], vertical_alignment="top")
                 with left:
-                    image = Image.open(member["image"])
+                    image = Image.open(imagelist[idx])
                     st.image(image, width=150)
                 with right:
                     st.markdown(f"## {member['name']}")
